2603522dd84a018f01f68556fb36f707230250e6
Victor Enogwe <victor.enogwe@gmail.com>
2016-10-14 22:43:21 +0100

    Add Comments To Each Test Case

--- tests.js
+++ tests.js
@@ -6,21 +6,25 @@ var assert = chai.assert;
 var lib = require('./lib/sum-of-primes.js');
 
 describe("Test that the sum of prime gives the correct answers for each test case", function(){
+  //interger Input Type Test Case
   it("should return an integer", function(){
     assert(
       typeOf(lib.sumOfPrimes(50)) == "number"
     );
   });
+  //Float Input Type Test Case
   it("should not accept float values", function(){
     assert(
       typeOf(lib.sumOfPrimes(50.33)) == "Please use an integer"
     );
   });
+  //PreEstablished Output Test Case
   it("should return 17 if the input is 10", function() {
     assert(
       lib.sumOfPrimes(10) == 17
     );
   });
+  //Check For Wrong Input Type
   it("should print 'wrong input type - <type>' for wrong inputs", function() {
     assert(
       lib.sumOfPrimes([]) == "wrong input type - array"
@@ -44,11 +48,13 @@ describe("Test that the sum of prime gives the correct answers for each test cas
       lib.sumOfPrimes(undefined) == "wrong input type - undefined"
     );
   });
+  //Test Case For Input Zero
   it("should return 'please use a number greater than zero' if input is zero", function() {
     assert(
       lib.sumOfPrimes(0) == "please use a number greater than zero"
     );
   });
+  //Test Case For Negative Numbers
   it("should return 'negative number not allowed'", function(){
   	assert(
         lib.sumOfPrimes(-10) == "Negative inputs not allowed"
